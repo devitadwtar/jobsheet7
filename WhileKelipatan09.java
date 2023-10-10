f2596e016b3b07dbdf855cd560acf6113945b606
devitadwtar <144086102+devitadwtar@users.noreply.github.com>
2023-10-10 14:40:43 +0700

    Add files via upload

--- WhileKelipatan09.java
+++ WhileKelipatan09.java
@@ -1,26 +1,29 @@
 import java.util.Scanner;
 
 public class WhileKelipatan09 {
-
     public static void main(String[] args) {
-        Scanner scan09 = new Scanner (System.in);
+        Scanner input09 = new Scanner(System.in);
 
-        int kelipatan, jumlah = 0, counter = 0;
-        double rataRata=0;
+        int kelipatan;
+        int jumlah = 0;
+        int counter = 0;
+        double rataRata = 0.0;
 
-        System.out.print("Masukkan bilangan kelipatan (1-9) ; ");
-        kelipatan = scan09.nextInt();
-        rataRata = scan09.nextDouble();
-        
-        if (int i = 1; i <= 50; i++) { 
+        System.out.print("Masukkan bilanhgan kelipatan (1 - 9) : ");
+        kelipatan =  input09.nextInt();
+
+        int i = 1;
+        while (i <= 50) {
             if (i % kelipatan == 0) {
                 jumlah += i;
-                counter++;
-                rataRata --;
-            }
-        }
-         System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
-         System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
-        System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, rataRata);
+                counter ++;
+            } 
+            i ++;
+            rataRata = (double)
+            jumlah / counter;
+         }
+        System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
+        System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+        System.out.printf("Rata-rata dari kelipatan %d dari 1 sampai 50 adalah %.2f\n", kelipatan, rataRata);
     }
 }
