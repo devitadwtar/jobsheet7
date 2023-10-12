085c3438273dfbc81c0a4514aaee404cf7c654dc
devitadwtar <144086102+devitadwtar@users.noreply.github.com>
2023-10-12 17:33:07 +0700

    Add files via upload

--- DeretBil09.java
+++ DeretBil09.java
@@ -1,17 +1,12 @@
-import java.util.Scanner;
 
 public class DeretBil09 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
 
-        System.out.print("Masukan bilangan : ");
-        int bil = input.nextInt();
-
-        for (int i = 1; i <= bil; i++) {
+        for (int i = 1; i <= 50; i++) {
             if (i % 3 == 0) {
                 continue;
             }
-            System.out.println("Bilangan yang bukan kelipatan 3 : " + i);
+            System.out.print(i + " ");
   }
 }
 }
